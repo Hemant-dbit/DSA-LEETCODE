23bc762b4e6511c2d9075bfa50b2f9ec3359ea8c
Hemant-dbit <guptahemant350@gmail.com>
2025-06-08 19:43:07 +0530

    Time: 1 ms (99.62%) | Memory: 45.3 MB (78.85%) - LeetSync

---
+++ 2032-largest-odd-number-in-string/largest-odd-number-in-string.java
@@ -0,0 +1,13 @@
+class Solution {
+    public String largestOddNumber(String num) {
+        
+        int len = num.length()-1;
+        for(int i=len;i>=0;i--){
+            if(num.charAt(i) %2 ==1){
+                return num.substring(0,i+1);
+            }
+        }
+
+        return "";
+    }
+}
