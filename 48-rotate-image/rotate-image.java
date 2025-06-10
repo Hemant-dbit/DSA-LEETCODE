97ad35dd8992b14c54c8a4e0ba75e50db145aca5
Hemant-dbit <guptahemant350@gmail.com>
2025-06-10 22:59:31 +0530

    Time: 0 ms (100.00%) | Memory: 42.4 MB (25.90%) - LeetSync

---
+++ 48-rotate-image/rotate-image.java
@@ -0,0 +1,27 @@
+class Solution {
+    public void rotate(int[][] matrix) {
+        int len = matrix.length;
+        int top = 0;
+        int bottom = len-1;
+
+        while(top<bottom){
+            for(int col=0;col<len;col++){
+                int temp = matrix[top][col];
+                matrix[top][col] = matrix[bottom][col];
+                matrix[bottom][col] = temp;
+            }
+            top++;
+            bottom--;
+        }
+
+        //Swapping non didagonal elements
+        for(int row = 0;row<len;row++){
+            for(int col = row+1; col<len;col++){
+                int temp = matrix[row][col];
+                matrix[row][col] = matrix[col][row];
+                matrix[col][row] = temp;
+            }
+        }
+        
+    }
+}
