b4a61b39a7fc5ddff3d85159a5e9fbda892992b5
Hemant-dbit <guptahemant350@gmail.com>
2025-06-11 23:43:25 +0530

    Time: 0 ms (100.00%) | Memory: 45.8 MB (44.43%) - LeetSync

---
+++ 73-set-matrix-zeroes/set-matrix-zeroes.java
@@ -0,0 +1,29 @@
+class Solution {
+    public void setZeroes(int[][] matrix) {
+        boolean FirstCol = false;
+
+        
+        for (int row = 0; row < matrix.length; row++) {
+            if (matrix[row][0] == 0) {
+                FirstCol = true;
+            }
+            for (int col = 1; col < matrix[0].length; col++) {
+                if (matrix[row][col] == 0) {
+                    matrix[row][0] = 0;
+                    matrix[0][col] = 0;
+                }
+            }
+        }
+
+        for (int row = matrix.length - 1; row >= 0; row--) {
+            for (int col = matrix[0].length - 1; col >= 1; col--) {
+                if (matrix[row][0] == 0 || matrix[0][col] == 0) {
+                    matrix[row][col] = 0;
+                }
+            }
+            if (FirstCol) {
+                matrix[row][0] = 0;
+            }
+        }
+    }
+}
